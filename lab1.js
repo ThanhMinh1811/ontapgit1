ad0720765222eae62377e833f0520785c7f00981
react-lab <0thanhminh0.tl@gmail.com>
2023-11-21 21:17:47 +0700

    sortrise

--- lab1.js
+++ lab1.js
@@ -1,4 +1,15 @@
 let arr=[4,2,3,1,9]
+for(let i=0; i<arr.length-1;i++){
+    for(let j=0; j<arr.length;j++){
+        if (arr[i]<arr[j]){
+            let temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        }
+    }
+}
+
+console.log(arr)
 
 
 let tong=0
